230340d685682cbcb8143f077cbf46a503a1348a
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-09-06 18:54:10 +0300

    ch01

---
+++ code-challenges/arrayReverse/array-reverse.js
@@ -0,0 +1,9 @@
+'use strict';
+function reverseArray (arr) {
+  let newArr = [];
+  for (let i = arr.length-1; i >= 0; i--){
+    newArr[newArr.length] = arr[i];
+  }
+  return newArr;
+}
+module.exports = reverseArray ;
